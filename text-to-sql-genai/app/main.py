9aaad3707dcd48cf91d8b09c71b977ce3f703405
Ayush Maurya <41903733+am15398@users.noreply.github.com>
2026-02-17 12:22:55 +0000

    add front end

--- text-to-sql-genai/app/main.py
+++ text-to-sql-genai/app/main.py
@@ -1,13 +1,24 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+from dotenv import load_dotenv
+
 from app.models import PromptRequest
 from app.llm import generate_sql
 from app.db import run_query
 
+load_dotenv()
+
 app = FastAPI()
 
+# Serve frontend
+app.mount("/static", StaticFiles(directory="frontend"), name="static")
+
+
 @app.get("/")
-def root():
-    return {"message": "Text-to-SQL GenAI API"}
+def serve_ui():
+    return FileResponse("frontend/index.html")
+
 
 @app.post("/query")
 def query_data(request: PromptRequest):
